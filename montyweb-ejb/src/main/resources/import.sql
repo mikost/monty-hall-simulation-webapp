5fd96cb5ef91c88bed159d842ab42e60bddcb4ca
Mikko Östlund <mikost@server.fake>
2015-08-28 22:19:19 +0200

    import.sql: Add user "user2" with password "pw2"

--- montyweb-ejb/src/main/resources/import.sql
+++ montyweb-ejb/src/main/resources/import.sql
@@ -5,4 +5,7 @@ CREATE TABLE Roles(id BIGINT, Role VARCHAR(64), RoleGroup VARCHAR(64), FOREIGN K
 INSERT INTO Principals (id, PrincipalID, Password) VALUES (hibernate_sequence.nextVal,'dbmonty', 'dbhall')
 SET @the_id=SELECT id from Principals where PrincipalID='dbmonty'
 INSERT INTO Roles VALUES (@the_id, 'uzers', 'Roles');
+INSERT INTO Principals (id, PrincipalID, Password) VALUES (hibernate_sequence.nextVal,'user2', 'pw2')
+SET @the_id=SELECT id from Principals where PrincipalID='user2'
+INSERT INTO Roles VALUES (@the_id, 'uzers', 'Roles');
 
