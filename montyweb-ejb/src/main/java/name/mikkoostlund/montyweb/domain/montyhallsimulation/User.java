dd85ff35e08b1746f8e5e950c1f8e0ca54c30d9f
Mikko Östlund <mikost@server.fake>
2015-08-29 12:45:14 +0200

    Fixed 2 compiler warnings.

--- montyweb-ejb/src/main/java/name/mikkoostlund/montyweb/domain/montyhallsimulation/User.java
+++ montyweb-ejb/src/main/java/name/mikkoostlund/montyweb/domain/montyhallsimulation/User.java
@@ -9,8 +9,6 @@ import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Column;
-import javax.persistence.Version;
-
 import java.lang.Override;
 
 @Entity
